1325215cf7dce92ec1fffca2c7327bc91555b959
Wataru Oguchi <wataru.oguchi.0to1@gmail.com>
2017-11-05 10:45:35 -0800

    Add a new page/component Metrics

---
+++ src/components/Metrics.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-2">
+                nav is here
+            </div>
+            <div class="col-md-10">
+                mock
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="coffee">
+export default
+    name: 'Metrics'
+</script>
+
+<style scoped src="bootstrap3/dist/css/bootstrap.min.css"></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Metrics from '@/components/Metrics'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: HelloWorld
+    },
+    {
+      path: '/metrics',
+      name: 'Metrics',
+      component: Metrics
     }
   ]
 })
